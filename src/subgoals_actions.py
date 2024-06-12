dc62e6820b9cc5f986c20378cab195bf74c03df3
Paul <ukpaulbd@gmail.com>
2024-06-12 01:26:23 -0400

    HTML add

---
+++ src/subgoals_actions.py
@@ -0,0 +1,39 @@
+
+SUBGOALS = [
+    {
+        'name': 'Abby wants to know where people are staying',
+        'link': 'https://2019.icse-conferences.org/info/icse-logo',
+        'actions': [
+            {
+                'action': 'Click on attending',
+                'link': 'https://2019.icse-conferences.org/info/icse-logo'
+            },
+            {
+                'action': 'Click on accommodation',
+                'link': 'https://2019.icse-conferences.org/info/icse-logo'
+            },
+            {
+                'action': 'Scroll Down',
+                'link': 'https://2019.icse-conferences.org/attending/hotel-registration'
+            }
+        ]
+    },
+    {
+        'name': 'Find out how many days to stay at the conference',
+        'link': 'https://2019.icse-conferences.org/info/icse-logo',
+        'actions': [
+            {
+                'action': 'Click on program',
+                'link': 'https://2019.icse-conferences.org/info/icse-logo'
+            },
+            {
+                'action': 'Click Week overview',
+                'link': 'https://2019.icse-conferences.org/info/icse-logo'
+            },
+            {
+                'action': 'Go up the page and click on tracks',
+                'link': 'https://2019.icse-conferences.org/info/week-overview'
+            }
+        ]
+    }
+]
