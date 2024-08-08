52072cf0bd7f2b75e42d358856a84177be8a86f6
Paul <ukpaulbd@gmail.com>
2024-08-08 11:23:20 -0400

    Gendermah

--- src/subgoals_actions.py
+++ src/subgoals_actions.py
@@ -1,70 +1,83 @@
 from constant import BASE_IMAGE_PATH
 
 
-TAG="PITTSBURG"
-gm_moments =  {
-    "scenario": "Find an issue to work on",
+TAG ="ROME"
+gm_moments= {
+    "scenario": "File an issue to have a merge request template",
     "Subgoals": [
         {
-            "subgoal": "Find list of issues/where issues are",
+            "subgoal": "Find information about how to file an issue",
             "question": "Will Abi have formed this sub-goal as a step to their overall goal?",
-            "subgoal_page_image":  BASE_IMAGE_PATH + "MOCKHomepage.png",
+            "subgoal_page_image": BASE_IMAGE_PATH + "Flohomepage.png",
             "Actions": [
                 {
-                    "action": "Scroll down the webpage",
+                    "action": "Read the readme",
                     "before_action": {
                         "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
-                        "page_image_on_which_abi_take_the_action":  BASE_IMAGE_PATH + "MOCKHomepage.png"
+                        "page_image_on_which_abi_take_the_action":  BASE_IMAGE_PATH + "Flohomepage.png",
                     },
                     "after_action": {
                         "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
-                        "page_image_on_which_abi_is_after_the_action":  BASE_IMAGE_PATH + "MOCKHomepage.png"
+                        "page_image_on_which_abi_is_after_the_action":  BASE_IMAGE_PATH + "Flohomepage.png",
                     }
                 },
                 {
-                    "action": "Click on the issue icon on the left side",
+                    "action": "Click on the contribute.md",
                     "before_action": {
                         "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
-                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH + "MOCKHomepage.png"
+                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH + "Flohomepage.png"
                     },
                     "after_action": {
                         "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
-                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH + "MOCKIssueList.png"
+                        "page_image_on_which_abi_is_after_the_action":  BASE_IMAGE_PATH + "Flocontribute.png"
                     }
                 }
             ]
         },
         {
-            "subgoal": "Selecting an issue to work on",
+            "subgoal": "File the issue",
             "question": "Will Abi have formed this sub-goal as a step to their overall goal?",
-            "subgoal_page_image": BASE_IMAGE_PATH + "MOCKIssueList.png",
+            "subgoal_page_image": BASE_IMAGE_PATH + "Flocontribute.png",
             "Actions": [
                 {
-                    "action": "Read through the list",
+                    "action": "Go to the list of issues",
                     "before_action": {
                         "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
-                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH + "MOCKIssueList.png"
+                        "page_image_on_which_abi_take_the_action":  BASE_IMAGE_PATH + "Flocontribute.png"
                     },
                     "after_action": {
                         "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
-                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH + "MOCKIssueList.png"
+                        "page_image_on_which_abi_is_after_the_action":  BASE_IMAGE_PATH + "MOCKIssueList.png"
                     }
                 },
                 {
-                    "action": "Select/click on an issue",
+                    "action": "Click 'new issue'",
                     "before_action": {
                         "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
                         "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH + "MOCKIssueList.png"
                     },
                     "after_action": {
                         "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
-                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH+ "Floissuedetails.png"
+                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH + "FloIssueCreate.png"
+                    }
+                },
+                {
+                    "action": "Fill out the form and submit the issue",
+                    "before_action": {
+                        "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
+                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH + "FloIssueCreate.png"
+                    },
+                    "after_action": {
+                        "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
+                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH + "FloAfterIsuueCreate.png"
                     }
                 }
             ]
         }
     ]
 }
+
+
 
 
 
