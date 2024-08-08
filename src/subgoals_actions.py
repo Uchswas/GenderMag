9a19318b9e66617e6f26cf13c7ff82131ba17a86
Paul <ukpaulbd@gmail.com>
2024-08-08 02:21:05 -0400

    Dhaka Session

--- src/subgoals_actions.py
+++ src/subgoals_actions.py
@@ -1,64 +1,67 @@
 from constant import BASE_IMAGE_PATH
 
-TAG = "MIAMI"
-gm_moments = {
-    "scenario": "Abi wants to submit Assignment 7",
+TAG = "DHAKA"
+gm_moments =  {
+    "scenario": "Create a new issue",
     "Subgoals": [
         {
-            "subgoal": "Find assignment 7",
+            "subgoal": "Find issue tracker",
             "question": "Will Abi have formed this sub-goal as a step to their overall goal?",
-            "subgoal_page_image" : BASE_IMAGE_PATH+"HomePage.png",
+            "subgoal_page_image": BASE_IMAGE_PATH + "JabrefHomepage.png",
             "Actions": [
                 {
-                    "action": "Click on assignment 7 link under Coming up section (Before)",
+                    "action": "Click on 'Issues' link",
                     "before_action": {
                         "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
-                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH+"HomePage.png",
-                     
+                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH + "JabrefHomepage.png"
                     },
                     "after_action": {
                         "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
-                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH+"Assignment7.png",
+                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH + "JabrefIssueList.png"
                     }
                 },
                 {
-                    "action": "Click on Github link (Before)",
+                    "action": "Click the 'New Issue' button",
                     "before_action": {
                         "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
-                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH+"Assignment7.png",
+                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH + "JabrefIssueList.png"
                     },
                     "after_action": {
                         "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
-                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH+"AfterclickingGithub.png",
-                    }
-                }
-            ]
-        },
-        {
-            "subgoal": "Go to Gradescope to submit assignment",
-            "question": "Will Abi have formed this sub-goal as a step to their overall goal?",
-            "subgoal_page_image" : BASE_IMAGE_PATH+"HomePage.png",
-            "Actions": [
-                {
-                    "action": "Click on assignment 7 link under Coming up section",
-                    "before_action": {
-                        "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
-                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH+"HomePage.png",
-                    },
-                    "after_action": {
-                        "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
-                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH+"Assignment7.png",
+                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH + "JabrefIssueList.png"
                     }
                 },
                 {
-                    "action": "Click on load assignment 7 for submission",
+                    "action": "Click the button 'Get Started' for 'Bug Report'",
                     "before_action": {
                         "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
-                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH+"Assignment7.png",
+                        "page_image_on_which_abi_take_the_action": "dummy_image_path/AfterClickingNewIssue.png"
                     },
                     "after_action": {
                         "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
-                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH+"Afterclickingload.png",
+                        "page_image_on_which_abi_is_after_the_action": "dummy_image_path/AfterClickingGetStarted.png"
+                    }
+                },
+                {
+                    "action": "Fill out the Bug Report form",
+                    "before_action": {
+                        "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
+                        "page_image_on_which_abi_take_the_action": "dummy_image_path/AfterClickingGetStarted.png"
+                    },
+                    "after_action": {
+                        "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
+                        "page_image_on_which_abi_is_after_the_action": "dummy_image_path/AfterFillingBugReport.png"
+                    }
+                },
+                {
+                    "action": "Click the button 'Submit new issue'",
+                    "before_action": {
+                        "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
+                        "page_image_on_which_abi_take_the_action": "dummy_image_path/AfterFillingBugReport.png"
+                    },
+                    "after_action": {
+                        "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
+                        "page_image_on_which_abi_is_after_the_action": "dummy_image_path/AfterSubmittingIssue.png"
                     }
                 }
             ]
