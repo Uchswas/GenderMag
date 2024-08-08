9042231a1941418c6bf5a410d2dd67987a265ca6
Paul <ukpaulbd@gmail.com>
2024-08-08 09:01:59 -0400

    IssueList

--- src/subgoals_actions.py
+++ src/subgoals_actions.py
@@ -1,24 +1,36 @@
 from constant import BASE_IMAGE_PATH
 
 
-TAG =  "RALEIGH",
+
+TAG="Lincoln"
 gm_moments = {
-    "scenario": "Set up the environment",
+    "scenario": "File an issue",
     "Subgoals": [
         {
-            "subgoal": "Gather more information on setting up the environment",
+            "subgoal": "Find information about filing an issue",
             "question": "Will Abi have formed this sub-goal as a step to their overall goal?",
-            "subgoal_page_image": BASE_IMAGE_PATH + "Flohomepage.png",
+            "subgoal_page_image": BASE_IMAGE_PATH + "MOCKHomepage.png",
             "Actions": [
                 {
-                    "action": "Scrolling to find information about this subgoal",
+                    "action": "Click on issue button on the side",
                     "before_action": {
                         "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
-                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH + "Flohomepage.png"
+                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH + "MOCKHomepage.png",
                     },
                     "after_action": {
                         "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
-                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH + "Flohomepage.png"
+                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH + "MOCKIssueList.png"
+                    }
+                },
+                {
+                    "action": "Click on the green new issue button",
+                    "before_action": {
+                        "question": "Will ABI know what to do at this step and is the page good enough for abi to take this action? Why?",
+                        "page_image_on_which_abi_take_the_action": BASE_IMAGE_PATH + "MOCKIssueList.png"
+                    },
+                    "after_action": {
+                        "question": "If Abi does the right thing, will s/he know that s/he did the right thing and is making progress toward their goal? Why?",
+                        "page_image_on_which_abi_is_after_the_action": BASE_IMAGE_PATH + "MOCKISSUECREATE.png"
                     }
                 }
             ]
